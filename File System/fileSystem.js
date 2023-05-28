0ae574ad9369ea107dea196cbce55956246eaf4d
Allen Farias <allenfarias29@gmail.com>
2023-05-28 05:07:58 -0700

    node:fs api - created folder and txt file

--- File System/fileSystem.js
+++ File System/fileSystem.js
@@ -1,16 +1,22 @@
 
-import * as fs from 'node:fs/promises';
+
+import * as fs from 'node:fs';
+import { mkdir } from 'node:fs/promises';
+
 
 // Note: when opening a file always close it as it can cause a memory leak
 
-let file;
+const content = "Here is some content";
 
 try {
-    file = await open();
-}
-
-
-
+    const projectFolder = new URL('./test/project/', import.meta.url);
+    await mkdir(projectFolder, { recursive: true }).then(console.log);
+    fs.writeFile('./test/project/random_file.txt', content, (err) => {
+        console.error("write: " + err);
+    });
+  } catch (err) {
+    console.error("dir: " + err.message);
+  }
 
 
 
