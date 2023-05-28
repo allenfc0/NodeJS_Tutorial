43fad74cb5737ad3cf147a300027ae3c9582ba9b
Allen Farias <allenfarias29@gmail.com>
2023-05-28 03:54:40 -0700

    checking out node:fs api

---
+++ File System/fileSystem.js
@@ -0,0 +1,22 @@
+
+import * as fs from 'node:fs/promises';
+
+// Note: when opening a file always close it as it can cause a memory leak
+
+let file;
+
+try {
+    file = await open();
+}
+
+
+
+
+
+
+
+
+
+
+
+
